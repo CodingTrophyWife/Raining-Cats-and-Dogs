f608af631f6bf1d2f1252e756477a023416da986
Cyndi Fosmark <cmfosmark@gmail.com>
2023-05-21 14:53:46 -0700

    added comments and touched up css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,66 +1,45 @@
-var FiveDayDisplayEL = $('#five-day-display');
-var fiveForecastMainEl = $('#five-day-display-main');
-var historyDisplayEL = $('#search-history-screen');
-var userInputEl = $('#city-selected');
-var searchBtnEl = $('#search-btn');
-var currentDayEl = $('#current-day');
-var currentDate =  dayjs().format(' [-] MMM DD, YYYY [-]');
+var FiveDayDisplay = $('#five-day-display');
+var fiveForecastMain = $('#five-day-display-main');
+var historyDisplay = $('#search-history-screen');
+var userInput = $('#city-selected');
+var searchBtn = $('#search-btn');
+var currentDay = $('#current-day');
+var currentDate = dayjs().format(' [-] MMM DD, YYYY [-]');
 
-
-//load first page hide items if search history stored show history
+// This will show all the previous searches from your local storage
 function loadHistoryList(){
-    // assign value if local storage has city history search
     var historyCity = readLocalStorage();
-
     if (historyCity){
-            for(i=0;i< historyCity.length ;i++){
-                var btnHistory = $('<button>');
-                btnHistory.addClass('btn btn-history');
-                btnHistory.text(historyCity[i]);
-                historyDisplayEL.append(btnHistory);
-                btnHistory.on('click',historyBtnClick);
-            }
-            historyDisplayEL.children().attr('style','visibility:visible');
-            
-
+        for(i=0;i< historyCity.length ;i++){
+            var btnHistory = $('<button>');
+            btnHistory.addClass('btn btn-history');
+            btnHistory.text(historyCity[i]);
+            historyDisplay.append(btnHistory);
+            btnHistory.on('click',historyBtnClick);
         }
+        historyDisplay.children().attr('style','visibility:visible');
+    }
 }
-//load last entered city in search history
-function loadLastItem(){
-    var historyCityList = readLocalStorage();
-    var lastCity = historyCityList.slice(-1);
-    var btnHistory = $('<button>');
-    btnHistory.addClass('btn btn-history');
-    btnHistory.text(lastCity);
-    historyDisplayEL.append(btnHistory);
-    historyDisplayEL.children().attr('style','visibility:visible');
-    btnHistory.on('click',historyBtnClick);
 
-}
-//load page when button history clicked
 function historyBtnClick(event){
     event.preventDefault();
     var currentClickedCity = event.currentTarget.firstChild.textContent.trim();
     getLatandLon(currentClickedCity);
 }
 
-// function to get search history from local 
 function readLocalStorage(){
     var localHistory = localStorage.getItem('cityHistory');
-  if (localHistory) {
-    localHistory = JSON.parse(localHistory);
-  } else {
-    localHistory = [];
-  }
-  return localHistory;
+    if (localHistory) {
+        localHistory = JSON.parse(localHistory);
+    } else {
+        localHistory = [];
+    }
+    return localHistory;
 }
 
-//call to the weather API to get info for current weather and forecast 
+// The weather API 
 function getLatandLon(cityName){
-   
-    var requestLatURL = 'https://api.openweathermap.org/geo/1.0/direct?q='+cityName+',US&limit=1&appid=f47e6a19d6e4c6e6b84b56bae2fdf11f'
-
-//fetch for latitude and longitude of city selected 
+    var requestLatURL = 'https://api.openweathermap.org/geo/1.0/direct?q='+cityName+',US&limit=1&appid=f47e6a19d6e4c6e6b84b56bae2fdf11f';
     fetch(requestLatURL)
     .then(function(response){
         return response.json();
@@ -68,22 +47,15 @@ function getLatandLon(cityName){
     .then(function(data){
         var latCity = data[0].lat;
         var lonCity = data[0].lon;
-        //give lat and lon to forecast api and current weather api
         var forecastRequest = 'https://api.openweathermap.org/data/2.5/forecast?lat='+latCity+'&lon='+lonCity+'&units=imperial&appid=f47e6a19d6e4c6e6b84b56bae2fdf11f';
         var currentWeather ='https://api.openweathermap.org/data/2.5/weather?lat='+latCity+'&lon='+lonCity+'&units=imperial&appid=f47e6a19d6e4c6e6b84b56bae2fdf11f';
-
-        // fetche current weather
         fetch(currentWeather)
         .then(function(currentResponse){
             return currentResponse.json();
-
         })
-        // display data in elements
         .then(function(currentData){
-
-            currentDayEl.children('h3').text(currentData.name);
-            currentDayEl.children('h3').append(currentDate);
-            //if more than one icon 
+            currentDay.children('h3').text(currentData.name);
+            currentDay.children('h3').append(currentDate);
             if(currentData.weather.length > 0 ){
                 for(var i=0; i < currentData.weather.length; i++){
                    var imageSrc = 'https://openweathermap.org/img/wn/'+currentData.weather[i].icon+'@2x.png';
@@ -91,67 +63,55 @@ function getLatandLon(cityName){
                    icon.attr('style', 'width:50px');
                    icon.attr('alt','weather icon');
                    icon.attr('src',imageSrc );
-                   currentDayEl.children('h3').append(icon);
+                   currentDay.children('h3').append(icon);
                 }
             }
-
-            currentDayEl.children().eq(1).text('Temp: '+currentData.main.temp+ '°F');
-            currentDayEl.children().eq(2).text('Wind: '+currentData.wind.speed+ ' MPH');
-            currentDayEl.children().eq(3).text('Humidity: '+currentData.main.humidity+' %');
+            currentDay.children().eq(1).text('Temp: '+currentData.main.temp+ '°F');
+            currentDay.children().eq(2).text('Wind: '+currentData.wind.speed+ ' MPH');
+            currentDay.children().eq(3).text('Humidity: '+currentData.main.humidity+' %');
         })
-        
-        // fetch five day forecast
         fetch(forecastRequest)
         .then(function(forecastResponse){
             return forecastResponse.json();
-
         })
         .then(function(forecastData){
              var listFive = [];
-             //incremented variable i by 8 to get just one time set for each day 
             for (var i=0; i < forecastData.list.length ; i+=8){
                 listFive.push(forecastData.list[i])
             }
-
             for (var i=0; i< listFive.length;i++){
                 var x= i+1
-                var divEl = $($('#day'+ x))
+                var div = $($('#day'+ x))
                 var date = dayjs(listFive[i]['dt_txt']).format('MMM DD, YYYY');
                 var imageSrc = 'https://openweathermap.org/img/wn/'+listFive[i].weather[0].icon+'@2x.png'
-
-                divEl.children('h5').text(date);
-                divEl.children('img').attr('src',imageSrc)
-                divEl.children().eq(2).text('Temp: ' + listFive[i].main.temp+ ' °F');
-                divEl.children().eq(3).text('Wind: ' + listFive[i].wind.speed+ ' MPH')
-                divEl.children().eq(4).text('Humidity: ' + listFive[i].main.humidity+ ' %');  
-            } 
-           
+                div.children('h5').text(date);
+                div.children('img').attr('src',imageSrc)
+                div.children().eq(2).text('Temp: ' + listFive[i].main.temp+ ' °F');
+                div.children().eq(3).text('Wind: ' + listFive[i].wind.speed+ ' MPH')
+                div.children().eq(4).text('Humidity: ' + listFive[i].main.humidity+ ' %');  
+            }   
         })
-        fiveForecastMainEl.attr('style','visibility:visible;');
-        currentDayEl.attr('style','visibility:visible;');
-    });
-    
+        fiveForecastMain.attr('style','visibility:visible;');
+        currentDay.attr('style','visibility:visible;');
+    });  
 }
 
-//search weather for city entered and button search clicked
+// this function is what searches for the city you typed in 
 function searchCityBtn(event){
     event.preventDefault();
-    
-    var citySelected = userInputEl.val();
+    var citySelected = userInput.val();
     getLatandLon(citySelected);
     saveToLocalStorage(citySelected);
     loadLastItem();
-    userInputEl.val('');
+    userInput.val('');
 }
-//save city to local storage 
+
+// this fuction will save your search to local storage 
 function saveToLocalStorage(cityName){
     var listCityHistory = readLocalStorage();
     listCityHistory.push(cityName);
     localStorage.setItem("cityHistory", JSON.stringify(listCityHistory));
 }
 
-// call when page loads 
 loadHistoryList()
-
-//search button click
-searchBtnEl.on('click',searchCityBtn)
+searchBtn.on('click',searchCityBtn)
